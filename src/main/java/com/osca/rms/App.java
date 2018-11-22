af6862db484870f194a2a37636fb4af4e87722e5
Pasindu Marasinghe <pasindu95.m@gmail.com>
2018-11-22 20:37:51 +0530

    Add local jar repository

--- src/main/java/com/osca/rms/App.java
+++ src/main/java/com/osca/rms/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         // /home/osca/Music/Anagathaye-Wayo-www.hirufm.lk.mp3
-        File file = new File("/home/osca/Music/Anagathaye-Wayo-www.hirufm.lk.mp3");
+        File file = new File("./temp/07  - Palayanne naa maa.mp3");
         FileUtil.mp3ToWav(file.getAbsolutePath(),"./temp/test");
         FormatConverter newConverter = new FormatConverter();
         List<AudioInputStream> convertFormat = newConverter.convertFormat(new File("./temp/test.wav"));
