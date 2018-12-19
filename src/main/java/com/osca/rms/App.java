7d99a20efa11ddba9ea2e7c0156941cdc3cc7a96
Pasindu Marasinghe <pasindu95.m@gmail.com>
2018-12-19 06:35:07 +0530

    Add Gson dependency

--- src/main/java/com/osca/rms/App.java
+++ src/main/java/com/osca/rms/App.java
@@ -1,8 +1,10 @@
 package com.osca.rms;
 
+import com.google.gson.Gson;
 import com.osca.rms.logic.audio.FeatureFinder;
 import com.osca.rms.logic.audio.FormatConverter;
 import com.osca.rms.util.FileUtil;
+import com.osca.rms.util.ResultBundle;
 
 import javax.sound.sampled.AudioInputStream;
 import java.io.File;
@@ -18,5 +20,8 @@ public class App {
         Map<Long, List<Integer>> hashMap = new FeatureFinder().extractFeaturesNew(convertFormat.get(0), 0, FeatureFinder.FFT_SHIFT_WIN_SIZE, true);
         System.out.println(hashMap.toString());
         new File("./temp/test.wav").delete();
+        ResultBundle<String> retPack = new ResultBundle<String>(ResultBundle.RETURN_TYPE_ERROR, "Connection Failure");
+        System.out.println(new Gson().toJson(hashMap));
+
     }
 }
